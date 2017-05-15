376bcb0a1d6bcab954a08f1dfba056900c503d6d
Nicolai Ramsby <nr@deveo.dk>
2017-05-15 22:18:04 +0200

    Correct folder-structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 
 Vue.use(Router)
 
@@ -9,7 +8,7 @@ export default new Router({
     {
       path: '/',
       name: 'Hello',
-      component: Hello
+      component: require('@/pages/index')
     }
   ]
 })
